2bf64a431f6756b98738c62fa3c88402d3e2fbbc
mengguotang <littlecandyi@163.com>
2020-09-10 17:26:05 +0800

    单独封装登陆接口

--- src/api/user.js
+++ src/api/user.js
@@ -1,8 +1,14 @@
 // 引入的是 utils下面的request.js 文件 本质上市一个axios
-// import request from '@/utils/request'
+import request from '@/utils/request'
 
 // 把所有的请求封装成一个个函数
+// 登录接口
 export function login(data) {
+  return request({
+    url: '/sys/login',
+    method: 'post',
+    data
+  })
 }
 
 export function getInfo(token) {
