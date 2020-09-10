a06f9f630f18c7bda207d59740b3428782006b99
mengguotang <littlecandyi@163.com>
2020-09-10 19:52:51 +0800

    封装Vuex中的登陆Actions / 简单处理token

--- src/store/getters.js
+++ src/store/getters.js
@@ -1,6 +1,7 @@
 const getters = {
   sidebar: state => state.app.sidebar,
-  device: state => state.app.device
-  // 删除原来的user模块
+  device: state => state.app.device,
+  // 导出 token
+  token: state => state.user.token
 }
 export default getters
--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -1,8 +1,41 @@
+// 引入设置token本地缓存的文件
+import { getToken, setToken, removeToken } from '@/utils/auth'
+// 引入封装的登录接口
+import { login } from '@/api/user'
+// 共享状态
+const state = {
+  token: getToken() // token的共享状态
+}
+// 修改共享状态(同步代码)
+const mutations = {
+  // 设置用户的 token 值
+  // 易错点 这里有两个 settoken 函数 , 但第一个是声明一个修改state token属性的函数名而已 可以的其他名字,里面的settoken调用是引入的本地缓存文件的调用,本质上是不一样
+  setToken(state, token) {
+    state.token = token
+    setToken(token)
+  },
+  // 删除 token
+  removeToken(state) {
+    state.token = null // 现将token的值设置为空
+    removeToken() // 删除
+  }
+}
+// 异步代码
+const actions = {
+  // 登录actions
+  // 这里的data 参数应该是调用actions的时候传过来的
+  async login(context, data) {
+    const res = await login(data)
+    if (res.data.success) {
+      // 如果接口响应成功应该吧数据调教mutations
+      context.commit('setToken', res.data.data)
+    }
+  }
+}
 
 export default {
   namespaced: true,
-  state: {},
-  mutations: {},
-  actions: {}
+  state,
+  mutations,
+  actions
 }
-
