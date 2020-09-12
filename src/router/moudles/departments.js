0fe8eae22ca4539da502e6367b6d3c62c145c2eb
mengguotang <littlecandyi@163.com>
2020-09-12 19:28:20 +0800

    设置菜单图标

--- src/router/moudles/departments.js
+++ src/router/moudles/departments.js
@@ -11,6 +11,7 @@ export default {
     component: () => import('@/views/dashboard')
   }],
   meta: {
-    title: '组织架构' // meta属性的里面的属性 随意定义 但是这里为什么要用title呢， 因为左侧导航会读取我们的路由里的meta里面的title作为显示菜单名称
+    title: '组织架构', // meta属性的里面的属性 随意定义 但是这里为什么要用title呢， 因为左侧导航会读取我们的路由里的meta里面的title作为显示菜单名称
+    icon: 'tree'
   }
 }
--- src/router/moudles/permission.js
+++ src/router/moudles/permission.js
@@ -11,6 +11,7 @@ export default {
     component: () => import('@/views/permission')
   }],
   meta: {
-    title: '权限管理' // meta属性的里面的属性 随意定义 但是这里为什么要用title呢， 因为左侧导航会读取我们的路由里的meta里面的title作为显示菜单名称
+    title: '权限管理', // meta属性的里面的属性 随意定义 但是这里为什么要用title呢， 因为左侧导航会读取我们的路由里的meta里面的title作为显示菜单名称
+    icon: 'lock'
   }
 }
