067c05864324cba0cc41b636c9a7321e125957c5
mengguotang <littlecandyi@163.com>
2020-09-14 17:11:10 +0800

    将数形组件的大致内容抽离封装成组件

---
+++ src/views/departments/components/tree-tools.vue
@@ -0,0 +1,47 @@
+<template>
+  <el-row type="flex" justify="space-between" align="middle" style="height: 40px;width: 100%">
+    <el-col>
+      <!-- 此时 data 应该换成 treeNode-->
+      <span>{{ treeNode.name }}</span>
+    </el-col>
+    <el-col :span="4">
+      <el-row type="flex" justify="end">
+        <el-col>{{ treeNode.manager }}</el-col>
+        <el-col>
+          <!-- 下拉菜单的提示文字 -->
+          <el-dropdown>
+            <span>
+              操作<i class="el-icon-arrow-down" />
+            </span>
+            <!-- 下拉菜单 -->
+            <el-dropdown-menu slot="dropdown">
+              <el-dropdown-item>添加子部门</el-dropdown-item>
+              <el-dropdown-item>编辑部门</el-dropdown-item>
+              <el-dropdown-item>删除部门</el-dropdown-item>
+            </el-dropdown-menu>
+          </el-dropdown>
+        </el-col>
+      </el-row>
+    </el-col>
+  </el-row>
+</template>
+
+<script>
+export default {
+  // 定义props
+  props: {
+    treeNode: {
+      type: Object, // 对象的类型
+      required: true // 要求父组件调用时必须要传一个 treeNode 属性,否则报错
+    },
+    isRoot: { // 控制组件显示与影藏
+      type: Boolean,
+      default: false // 因为节点用的多 所以默认值给false
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
--- src/views/departments/index.vue
+++ src/views/departments/index.vue
@@ -1,55 +1,12 @@
 <template>
   <div class="dashboard-container">
     <div class="app-container">
+      <!-- 卡片组件  页面基本布局 -->
       <el-card class="tree-card">
         <!-- element 行列布局 -->
-        <el-row type="flex" justify="space-between" align="middle" style=" height: 40px;">
-          <el-col>
-            <span>江苏传智播客教育科技股份有限公司</span>
-          </el-col>
-          <el-col :span="4">
-            <el-row type="flex" justify="end">
-              <el-col>负责人</el-col>
-              <el-col>
-                <!-- 点击 下拉菜单 -->
-                <el-dropdown>
-                  <span>
-                    操作<i class="el-icon-arrow-down" />
-                  </span>
-                  <el-dropdown-menu slot="dropdown">
-                    <el-dropdown-item>
-                      添加子部门
-                    </el-dropdown-item>
-                  </el-dropdown-menu>
-                </el-dropdown>
-              </el-col>
-            </el-row>
-          </el-col>
-        </el-row>
+        <tree-tools :tree-node="company" :is-root="true" />
         <el-tree :data="departs" :props="defaultProps" default-expand-all>
-          <el-row slot-scope="{ data }" type="flex" justify="space-between" align="middle" style="height: 40px;width: 100%">
-            <el-col>
-              <span>{{ data.name }}</span>
-            </el-col>
-            <el-col :span="4">
-              <el-row type="flex" justify="end">
-                <el-col>{{ data.manager }}</el-col>
-                <el-col>
-                  <!-- 下拉菜单 -->
-                  <el-dropdown>
-                    <span>
-                      操作<i class="el-icon-arrow-down" />
-                    </span>
-                    <el-dropdown-menu>
-                      <el-dropdown-item>添加子部门</el-dropdown-item>
-                      <el-dropdown-item>编辑部门</el-dropdown-item>
-                      <el-dropdown-item>删除部门</el-dropdown-item>
-                    </el-dropdown-menu>
-                  </el-dropdown>
-                </el-col>
-              </el-row>
-            </el-col>
-          </el-row>
+          <tree-tools slot-scope="{ data }" :tree-node="data" />
         </el-tree>
       </el-card>
     </div>
@@ -57,14 +14,22 @@
 </template>
 
 <script>
+import TreeTools from './components/tree-tools'
 export default {
+  components: {
+    TreeTools
+  },
   data() {
     return {
       defaultProps: {
         label: 'name'
       },
       departs: [{ name: '总裁办', children: [{ name: '董事会' }] },
-        { name: '行政部' }, { name: '人事部' }]
+        { name: '行政部' }, { name: '人事部' }],
+      company: {
+        name: '江苏传智播客教育科技股份有限公司',
+        manager: '负责人'
+      }
     }
   }
 }
