59f5005ef68a06101c1da81ac01f70b63931fdaf
mengguotang <littlecandyi@163.com>
2020-09-17 16:34:18 +0800

    员工页面的数据渲染

--- src/api/employees.js
+++ src/api/employees.js
@@ -1,12 +1,24 @@
 import request from '@/utils/request'
 
 /**
-* @description: 获取员工的简单裂变
+* @description: 获取员工的简单列表
 * @param {type}
 * @return:
 */
 export function getEmployeeSimple() {
   return request({
     url: '/sys/user/simple'
+  })
+}
+
+/**
+* @description: 获取员工的列表数据
+* @param {Object} params query参数 分页数据对象
+* @return: request 对象
+*/
+export function getEmployeesList(params) {
+  return request({
+    url: '/sys/user',
+    params
   })
 }
--- src/views/employees/index.vue
+++ src/views/employees/index.vue
@@ -1,9 +1,9 @@
 <template>
-  <div class="dashboard-container">
+  <div v-loading="loading" class="dashboard-container">
     <div class="app-container">
       <!-- 顶部栏 -->
       <page-tools :show-before="true">
-        <span slot="before">共12条记录</span>
+        <span slot="before">共{{ page.total }}条记录</span>
         <template slot="after">
           <el-button size="small" type="warning">导入</el-button>
           <el-button size="small" type="danger">导出</el-button>
@@ -12,16 +12,60 @@
       </page-tools>
       <!-- 主体表格分页 -->
       <el-card>
-        <el-table>
-          <el-table-column label="序号" sortable="" />
-          <el-table-column label="姓名" sortable="" />
-          <el-table-column label="工号" sortable="" />
-          <el-table-column label="聘用形式" sortable="" />
-          <el-table-column label="部门" sortable="" />
-          <el-table-column label="入职时间" sortable="" />
-          <el-table-column label="账号状态" sortable="" />
+        <el-table
+          border=""
+          :data="list"
+        >
+          <el-table-column
+            label="序号"
+            sortable=""
+            type="index"
+          />
           <el-table-column
             header-align="center"
+            align="center"
+            label="姓名"
+            sortable=""
+            prop="username"
+          />
+          <el-table-column
+            header-align="center"
+            align="center"
+            label="工号"
+            sortable=""
+            prop="workNumber"
+          />
+          <el-table-column
+            header-align="center"
+            align="center"
+            label="聘用形式"
+            sortable=""
+            prop="formOfEmployment"
+          />
+          <el-table-column
+            header-align="center"
+            align="center"
+            label="部门"
+            sortable=""
+            prop="departmentName"
+          />
+          <el-table-column
+            header-align="center"
+            align="center"
+            label="入职时间"
+            sortable=""
+            prop="timeOfEntry"
+          />
+          <el-table-column
+            header-align="center"
+            align="center"
+            label="账号状态"
+            sortable=""
+            prop="enableState"
+          />
+          <el-table-column
+            header-align="center"
+            align="center"
             label="操作"
             fixed="right"
             width="280"
@@ -62,7 +106,13 @@
           align="middle"
           style="height: 60px"
         >
-          <el-pagination layout="prev,pager,next" />
+          <el-pagination
+            layout="prev,pager,next"
+            :page-size="page.size"
+            :current-page="page.page"
+            :total="page.total"
+            @current-change="changePage"
+          />
         </el-row>
       </el-card>
     </div>
@@ -70,11 +120,37 @@
 </template>
 
 <script>
+import { getEmployeesList } from '@/api/employees'
 export default {
-
+  data() {
+    return {
+      loading: false,
+      list: [],
+      page: {
+        page: 1,
+        size: 10,
+        total: 0
+      }
+    }
+  },
+  created() {
+    this.getEmployeesList()
+  },
+  methods: {
+    changePage(newPage) {
+      this.page.page = newPage // 设置页码
+      this.getEmployeesList() // 重新拉取数据
+    },
+    async getEmployeesList() {
+      this.loading = true // 数据请求回来之前显示loading
+      const { total, rows } = await getEmployeesList(this.page)
+      this.page.total = total // 赋值总条数
+      this.list = rows // 所有的列表数据
+      this.loading = false // 隐藏
+    }
+  }
 }
 </script>
 
 <style lang="less" scoped>
-
 </style>
